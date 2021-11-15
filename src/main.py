ea1857f2b78fd842a89956792dfc35d226f4dbe9
Folkenciyo <folkencillo@gmail.com>
2021-11-15 17:08:05 +0000

    species

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, Species, SpeciesDetails
 #from models import Person
 
 app = Flask(__name__)
@@ -25,19 +25,49 @@ setup_admin(app)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
 
+
 # generate sitemap with all your endpoints
 @app.route('/')
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+""" @app.route('/user', methods=['GET'])
+def get_user():
+    users = User.get_all()
 
     return jsonify(response_body), 200
+
+
+@app.route('/user/<int:id>', methods=['GET'])
+def get_user_by_id(id):
+    user = User.get_by_id(id)
+
+    if user:
+        return jsonify(user.to_dict()), 200
+
+    return jsonify({'message':'User not found'}), 404     """
+
+@app.route('/species/', methods=['GET'])
+def get_all_species():
+    species = Species.get_all()
+
+    if species:
+        return jsonify(species.to_dict()), 200
+
+    return jsonify({'message':'Species not found'}), 400
+
+
+@app.route('/species/<int:id>', methods=['GET'])
+def get_species(id):
+    all_species = Species.get_by_id(id)
+
+    if all_species:
+        return jsonify(species.to_dict()), 200
+
+    return jsonify({'message':'Species not found'}), 400
+
+
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -2,18 +2,89 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class User(db.Model):
+""" class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
+    nick = db.Column(db.String(120), unique=True, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    species = db.relationship('species', lazy=True)
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return f'User is {self.nick}, id:{self.id}'
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "nick": self.nick,
+        }
+
+    def get_all():
+        users = query.all()
+        return users
+     """
+class Species(db.Model):
+    __tablename__:"species"
+    id = db.Column(db.Integer, primary_key=True)
+    """ user_id = db.Column(db.Integer, db.ForeignKey("User.id")) """
+    name = db.Column(db.String, unique=False, nullable=False)
+    url = db.Column(db.String, unique=False, nullable=False)
+
+    def repr(self):
+        return f'Species: {self.id}, url: {self.url}'
 
     def serialize(self):
         return {
             "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
+            "name": self.name,
+            "url": self.url
         }
+
+    @classmethod    
+    def get_all(cls):
+        species_list = cls.query.all()
+        return [species.serialize() for i in species_list]
+
+    @classmethod
+    def get_by_id(cls, id):
+        specie = cls.query.get(id)
+        return specie
+
+
+class SpeciesDetails(db.Model):
+    __tablename__:"speciesDetails"
+    id = db.Column(db.Integer, primary_key=True)
+    classification = db.Column(db.String(250), unique=False, nullable=False)
+    designation = db.Column(db.String(250),unique=False, nullable=False)
+    average_height = db.Column(db.Integer, unique=False, nullable=False)
+    average_lifespan = db.Column(db.Integer, unique=False, nullable=False)
+    hair_color = db.Column(db.String(250), unique=False, nullable=False)
+    eye_color = db.Column(db.String(250), unique=False, nullable=False)
+    homeworld = db.Column(db.String(250), unique=False, nullable=False)
+    language = db.Column(db.String(250), unique=False, nullable=False)
+    people = db.Column(db.String(250), unique=False, nullable=False)
+    created = db.Column(db.String(250), unique=False, nullable=False)
+    edited = db.Column(db.String(250), unique=False, nullable=False)
+    name = db.Column(db.String(250), unique=False, nullable=False)
+    url = db.Column(db.String(250), unique=False, nullable=False)
+
+    """ def __repr__(self):
+        return "<SpeciesDetails>" % self.username """
+        
+    def serialize(self):
+        return {
+            "id" : self.id,
+            "classificacion": self.classificacion,
+            "designation": self.designation,
+            "average_height": self.average_height,
+            "average_lifespan": self.average_lifespan,
+            "hair_color": self.hair_color,
+            "eye_color": self.eye_color,
+            "homeworld": self.homeworld,
+            "language": self.language,
+            "people": self.people,
+            "created": self.created,
+            "edited": self.edited,
+            "name": self.name,
+            "url": self.url
+        }
+   
+     
