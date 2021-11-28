710b03b3e9a091127d40b9e677ddf6e258308a35
Folkenciyo <folkencillo@gmail.com>
2021-11-28 15:30:48 +0000

    update all working properly

--- src/main.py
+++ src/main.py
@@ -2,17 +2,20 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 import os
-import datetime
+from datetime import timedelta, datetime
 
+
+from datetime import timedelta, datetime
 from flask import Flask, request, jsonify, url_for
 from flask_migrate import Migrate
 from flask_swagger import swagger
 from flask_cors import CORS
-from flask_jwt_extended import create_access_token, get_jwt_identity, jwt_required ,JWTManager
-
+from sqlalchemy import exc
+from flask_jwt_extended import create_access_token, get_jwt_identity, jwt_required, JWTManager
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, Species, SpeciesDetails, User
+from sqlalchemy import exc
+from models import db, User, FavouritePeople, People, PeopleDetail, Species, SpeciesDetails
 
 #from models import Person
 
@@ -20,15 +23,16 @@ app = Flask(__name__)
 app.url_map.strict_slashes = False
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_CONNECTION_STRING')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config["JWT_SECRET_KEY"] = os.environ.get('JWT_KEY')  # Change this!
+app.config["JWT_SECRET_KEY"] = os.environ.get('JWT_KEY')
 jwt = JWTManager(app)
+
 
 MIGRATE = Migrate(app, db)
 db.init_app(app)
 CORS(app)
 setup_admin(app)
 
-# Handle/serialize errors like a JSON object
+# Handle/to_dict errors like a JSON object
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
@@ -45,14 +49,15 @@ def login():
     email, password = request.json.get('email', None), request.json.get('password', None)
 
     if email and password:
-        user= User.get_by_email(email)
+        user = User.get_by_email(email)
 
         if user:
-            """check PASSWORD"""
-            access_token = create_access_token(identity=user.id, datetime=timedelta(hours=12))
+            password = User.get_by_password(password)
+            access_token = create_access_token(identity=user.to_dict(), expires_delta=timedelta(days=30))
             return jsonify({'token': access_token}), 200
-
-    return jsonify({'error':'Invalid Data'}), 400
+    
+        return jsonify({'error': 'Invalid information'}), 400
+    return jsonify({"msg": "wrong info"})
 
 
 @app.route('/user', methods=['GET'])
@@ -62,37 +67,85 @@ def get_user():
     return jsonify(users.to_dict()), 200
 
 
-@app.route('/user/<int:id>/favourite', methods=['GET'])
-@jwt_required
-def get_fav(id):
-    user = User.get_by_id(id)
-    token_id = get_jwt_identity
+@app.route('/user', methods=['POST'])
+def create_user():
+    new_email = request.json.get('email', None)
+    new_username = request.json.get('username', None)
+    new_password = request.json.get('password', None)
 
-    if token_id == id:
-        pass
-        """return favs"""
+    if not (new_email and new_username and new_password):
+        return jsonify({'error': 'Missing user'}), 400
 
-    return jsonify({'message':'User not found'}), 404    
+    user_created = User(email=new_email, username=new_username, _password=new_password) 
+
+    try:
+        user_created.create()
+    except exc.IntegrityError:
+        return jsonify({'error': 'Fail in creating user'}), 400
+    
+    account = User.get_by_email(new_email)
+    access_token = create_access_token(identity=account.to_dict(), expires_delta=timedelta(days=30))
+    return jsonify({'token': access_token}), 200
+
 
 @app.route('/species/', methods=['GET'])
 def get_all_species():
-    species = Species.get_all()
+    specie_s = Species.get_all()
 
-    if species:
-        return jsonify(species.to_dict()), 200
+    if specie_s:
+        all_species = [species.to_dict() for species in specie_s]
+        return jsonify(all_species), 200
 
     return jsonify({'message':'Species not found'}), 400
 
 
-@app.route('/species/<int:id>', methods=['GET'])
+@app.route('/species/<int:id>/speciesdetails', methods=['GET'])
 def get_species(id):
-    all_species = Species.get_by_id(id)
+    all_species = Species.get_by_id_speciesdetails(id)
 
     if all_species:
-        return jsonify(species.to_dict()), 200
+        return jsonify(all_species.to_dict()), 200
 
     return jsonify({'message':'Species not found'}), 400
 
+@app.route('/user/<int:id_user>/favourite-species/<int:id_species>', methods=['POST'])
+@jwt_required()
+def add_favspecies(id_user,id_species):
+    token_id = get_jwt_identity()
+    print(token_id)
+
+    if token_id.get("id") == id_user:
+        user = User.get_user_by_id(id_user)
+        species = Species.get_by_id(id_species)
+        
+        if user and species:
+            add_fav = user.add_fav_species(species)
+            fav_species = [species.to_dict() for species in add_fav]
+            return jsonify(fav_species),200
+
+    return jsonify({'error': 'Not authorized'}),404
+
+    
+@app.route('/people', methods=['GET'])
+def get_all_people():
+    characters = People.get_all()
+
+    if characters:
+        character_list = [character.to_dict() for character in characters] 
+        return jsonify({character_list}), 200
+
+
+    return jsonify({'error': 'Characters not found'}), 400
+
+
+@app.route('/people/<int:id>', methods=['GET'])
+def get_character(id):
+    character = People.get_by_id(id)
+
+    if character:
+        return jsonify(character.to_dict()), 200
+
+    return jsonify({'error': 'Character not found'})
 
 
 # this only runs if `$ python src/main.py` is executed
--- src/models.py
+++ src/models.py
@@ -2,80 +2,120 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+speciesfavourites = db.Table('speciesfavourites',
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id'), primary_key=True),
+    db.Column('species_id', db.Integer, db.ForeignKey('species.id'), primary_key=True)
+)
+
 class User(db.Model):
+    __tablename__: 'user'
+
     id = db.Column(db.Integer, primary_key=True)
-    nick = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
-    
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    username = db.Column(db.String(120), unique=True, nullable=False)
+    _password = db.Column(db.String(80), unique=False, nullable=False)
+    _is_active = db.Column(db.Boolean(), unique=False, nullable=False, default=True)
+
+    have_user_species = db.relationship(
+        'Species', 
+        secondary = speciesfavourites, 
+        back_populates="have_user_speciesfav"
+        )
 
     def __repr__(self):
-        return f'User {self.email}'
+        return f'User is {self.username}, with {self.email} and {self.id}'
+    
 
     def to_dict(self):
         return {
             "id": self.id,
             "email": self.email,
-            "password": self.password
+            "username": self.username,
+            "species": [species.to_dict() for species in self.have_user_species]
         }
-    
-    def get_all():
-        users = query.all()
+
+    def create(self):
+       db.session.add(self)
+       db.session.commit()
+
+        
+    @classmethod
+    def get_by_email(cls, email):
+        account = cls.query.filter_by(email=email).one_or_none()
+        return account
+
+
+    @classmethod
+    def get_by_password(cls, password):
+        secretPass = cls.query.filter_by(password=password).one_or_none()
+        return secretPass
+
+
+    @classmethod
+    def get_all(cls):
+        users = cls.query.all()
         return users
+
+    @classmethod
+    def get_user_by_id(cls,id):
+        user = cls.query.get(id)
+        return user
     
+    def add_fav_species(self,specie):
+        self.have_user_species.append(specie)
+        db.session.commit()
+        return self.have_user_species
+
+
 class Species(db.Model):
     __tablename__:"species"
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String, unique=False, nullable=False)
-    detail_id = db.Column(db.Integer, db.ForeignKey("species_details.id"), nullable=False)
+    name = db.Column(db.String(250), unique=False, nullable=False)
+    species_id = db.Column(db.Integer, db.ForeignKey("species_details.id"), nullable=False)
 
-    people_has_detail = db.relationship("SpeciesDetails", back_populates="detail_has_specie")
+    species_have = db.relationship("SpeciesDetails", back_populates = "species_have_details")
+    have_user_speciesfav= db.relationship('User', secondary= speciesfavourites, back_populates="have_user_species")
 
-    
-    def repr(self):
-        return f'Species: {self.id}, url: {self.url}'
+    def __repr__(self):
+        return f'Species: {self.id}, name: {self.name}'
 
-    def serialize(self):
+    def to_dict(self):
         return {
             "id": self.id,
             "name": self.name,
         }
-
+        
     @classmethod    
     def get_all(cls):
-        species_list = cls.query.all()
-        return [species.serialize() for i in species_list]
+        species = cls.query.all()
+        return species
 
     @classmethod
     def get_by_id(cls, id):
-        specie = cls.query.get(id)
-        return specie
-
+        species = cls.query.get(id)
+        return species
 
 class SpeciesDetails(db.Model):
     __tablename__:"species_details"
     id = db.Column(db.Integer, primary_key=True)
     classification = db.Column(db.String(250), unique=False, nullable=False)
     designation = db.Column(db.String(250),unique=False, nullable=False)
-    average_height = db.Column(db.FLOAT, unique=False, nullable=False)
+    average_height = db.Column(db.Integer, unique=False, nullable=False)
     average_lifespan = db.Column(db.Integer, unique=False, nullable=False)
     hair_color = db.Column(db.String(250), unique=False, nullable=False)
     eye_color = db.Column(db.String(250), unique=False, nullable=False)
     homeworld = db.Column(db.String(250), unique=False, nullable=False)
     language = db.Column(db.String(250), unique=False, nullable=False)
     people = db.Column(db.String(250), unique=False, nullable=False)
-    created = db.Column(db.DATETIME(250), unique=False, nullable=False)
-    edited = db.Column(db.DATETIME(250), unique=False, nullable=False)
     name = db.Column(db.String(250), unique=False, nullable=False)
 
-    detail_has_specie = db.relationship("Species", back_populates="people_has_detail")
-    #people_has_detail = db.relationship("SpeciesDetails", back_populates="detail_has_specie")
+    species_have_details = db.relationship('Species', back_populates="species_have")
 
-    """ def __repr__(self):
-        return "<SpeciesDetails>" % self.username """
+    def __repr__(self):
+        return f'SpeciesDetails is {self.classificacion}, classification:{self.classificacion}, designation:{self.designation}, average_height:{average_height}, average_lifespan:{self.average_lifespan}, hair_color:{self.hair_color}, eye_color:{self.eye_color}, homeworld:{self.homeworld}, language:{self.language}, people:{self.people}, name:{self.name}' 
         
-    def serialize(self):
+    def to_dict(self):
         return {
             "id" : self.id,
             "classificacion": self.classificacion,
@@ -87,9 +127,96 @@ class SpeciesDetails(db.Model):
             "homeworld": self.homeworld,
             "language": self.language,
             "people": self.people,
-            "created": self.created,
             "edited": self.edited,
             "name": self.name,
         }
+    
+    @classmethod
+    def get_all_speciesdetails(cls):
+        speciesdetails = cls.query.all()
+        return speciesdetails
+    
+    @classmethod
+    def get_by_id_speciesdetails(cls,id):
+        speciesdetails = cls.query.get(id)
+        return speciesdetails
+
    
-     
+class FavouritePeople(db.Model):
+    __tablename__: "favourite_people"
+
+    id = db.Column(db.Integer, primary_key=True)
+    people_id = db.Column(db.Integer, db.ForeignKey("people.id"), nullable=False)
+
+
+class People(db.Model):
+    __tablename__: "people"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), unique=False, nullable=False)
+    detail_id = db.Column(db.Integer, db.ForeignKey("people_detail.id"), nullable=False)
+
+    people_has_details = db.relationship("PeopleDetail", back_populates="detail_has_character")
+
+    def __repr__(self):
+        return f'People is {self.name}, url: {self.url}'
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name
+        }
+
+
+    @classmethod
+    def get_all(cls):
+        character_list = cls.query.all()
+        return character_list
+
+
+    @classmethod
+    def get_by_id(cls, id):
+        character = cls.query.get(id)
+        return character  
+
+
+class PeopleDetail(db.Model):
+    __tablename__: "people_detail" 
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), unique=False, nullable=False)
+    height = db.Column(db.FLOAT, unique=False, nullable=False)
+    mass = db.Column(db.FLOAT, unique=False, nullable=False)
+    hair_color = db.Column(db.String(250), unique=False, nullable=False)
+    skin_color = db.Column(db.String(250), unique=False, nullable=False)
+    eye_color = db.Column(db.String(250), unique=False, nullable=False)
+    birth_year = db.Column(db.DATETIME(timezone=False), unique=False, nullable=False) 
+    gender = db.Column(db.String(250), unique=False, nullable=False)
+    created = db.Column(db.DATETIME(timezone=False), unique=False, nullable=False)
+    edited =db.Column(db.DATETIME(timezone=False), unique=False, nullable=False)
+    homeworld = db.Column(db.String(250), unique=False, nullable=False)
+
+    detail_has_character = db.relationship("People", back_populates="people_has_details")
+
+    # def __repr__(self):
+    #     return '<PeopleDetail>' % self.username
+
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "height": self.height,
+            "mass": self.mass,
+            "hair_color": self.hair_color,
+            "skin_color": self.skin_color,
+            "eye_color": self.eye_color,
+            "birth_year": self.birth_year,
+            "gender": self.gender,
+            "created": self.created,
+            "edited": self.edited,
+            "homeworld": self.homeworld
+        }
+
+
+        
