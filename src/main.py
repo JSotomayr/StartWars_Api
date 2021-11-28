a1246c748d18d0a77e0fd7b25b6923650a02f295
AnnaLemons <a.lopezdelg@gmail.com>
2021-11-28 18:16:47 +0000

    annaLemons

--- src/main.py
+++ src/main.py
@@ -2,19 +2,31 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 import os
+from datetime import timedelta, datetime
+
 from flask import Flask, request, jsonify, url_for
 from flask_migrate import Migrate
 from flask_swagger import swagger
 from flask_cors import CORS
+
+from flask_jwt_extended import create_access_token, get_jwt_identity, jwt_required, JWTManager
+from sqlalchemy import exc
+
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, Starship, StarshipDetail, User
+
 #from models import Person
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_CONNECTION_STRING')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+
+app.config["JWT_SECRET_KEY"] = os.environ.get("JWT_KEY")
+jwt = JWTManager(app)
+
 MIGRATE = Migrate(app, db)
 db.init_app(app)
 CORS(app)
@@ -30,14 +42,111 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
+    # LOGIN
+@app.route("/login", methods=["POST"])
+def login():
+    email, password = request.json.get('email', None), request.json.get('password', None)
+
+    if email and password:
+        user = User.get_by_email(email)
+
+        if user:
+            access_token = create_access_token(identity=user.to_dict(), expires_delta=timedelta(days=15))
+            return jsonify({'token': access_token}), 200
+
+    return jsonify({'error': 'Invalid info'}), 400
+
+    # get USER
 @app.route('/user', methods=['GET'])
-def handle_hello():
+def get_user():
+    users = User.get_all()
+    all_users=[user.to_dict() for user in users]
+    return jsonify(all_users), 200
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+    # post USER
+@app.route('/user', methods=['POST'])
+def create_user():
+    new_email=request.json.get('email', None)
+    new_user=request.json.get('username', None)
+    new_password=request.json.get('password', None)
 
-    return jsonify(response_body), 200
+    if not (new_user and new_email and new_password):
+        return jsonify({'error':'Missing user'}), 400
+
+    user_created= User(username=new_user, email=new_email, _password=new_password)
+
+    try:
+        user_created.create()
+    except exc.IntegrityError:
+        return jsonify({'error': 'Fail in data'}), 400
+
+    account = User.get_by_email(new_email)
+    access_token = create_access_token(identity=account.to_dict(), expires_delta=timedelta(days=30))
+    return jsonify({'token': access_token}), 200
+
+                # GET USER BY ID
+
+@app.route('/user/<int:id>', methods=['GET'])
+def get_user_by_id(id):
+    user = User.get_by_id(id)
+
+    if user:
+        return jsonify(user.to_dict()), 200
+
+        return jsonify({'error': 'User is not found'}), 404
+
+@app.route('/user/<int:id>/favourite', methods=['GET'])
+@jwt_required
+def get_fav(id):
+    token_id = get_jwt_identity()
+
+    if token_id == id:
+        favourites = Favourite.get_all()
+        return favourite
+
+    return jsonify({'error': 'You are not authorized'})
+
+
+    # GET STARSHIP
+@app.route('/starship', methods=['GET'])
+def get_starships():
+    starships = Starship.get_all()
+
+    if starships:
+        starship_list = [starship.to_dict() for starship in starships]
+        return jsonify(starship_list), 200
+
+    return jsonify({'error': 'Starship not found'}), 404
+
+
+            #GET BY (STARSHIPS ID)
+@app.route('/starship/<int:id>', methods=['GET'])
+def get_starship_by_id(id):
+    starship = Starship.get_by_id(id)
+    if starship:
+        return jsonify(starship.to_dict()), 200
+
+    return jsonify({'error':"Starship not found"}), 404
+
+
+@app.route('/user/<int:id_user>/favourite-starship/<int:id_starship>', methods=['POST'])
+@jwt_required()
+def add_favstarship(id_user,id_starship):
+    token_id = get_jwt_identity()
+
+    if token_id.get("id") == id_user:
+        user = User.get_by_id(id_user)
+        starship = Starship.get_by_id(id_starship)
+
+        if user and starship:
+            add_fav = user.add_fav_starship(starship)
+            fav_starships = [starship.to_dict() for starship in add_fav]
+            return jsonify(fav_starships), 200
+
+    return jsonify({'error': 'Starship favorite not found'}), 404
+
+
+
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -2,18 +2,118 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+starshipfavourites = db.Table('starshipfavourites',
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id'), primary_key=True),
+    db.Column('starship_id', db.Integer, db.ForeignKey('starship.id'), primary_key=True)
+)
+
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    __tablename__: 'user'
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(250), unique = True, nullable = False)
+    email = db.Column(db.String(250), unique = True, nullable = False)
+    _password = db.Column(db.String(250), unique = True, nullable = False)
+    _is_active = db.Column(db.String(250), unique = True, nullable = False, default = True)
+
+    have_user_starship = db.relationship('Starship', secondary=starshipfavourites, back_populates="have_user_starshipfav")
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return f'User is id:{self.d}, email:{self.email}, username:{self.username}, _password:{self.password}, _is_active:{self._is_active}'
 
-    def serialize(self):
+    def to_dict(self):
         return {
             "id": self.id,
+            "username":self.username,
             "email": self.email,
+            "starships": [starship.to_dict() for starship in self.have_user_starship]
+        }
+
+    def create(self):
+        db.session.add(self)
+        db.session.commit()
+        
+
+    @classmethod
+    def get_by_email(cls, email):
+        account = cls.query.filter_by(email=email).one_or_none()
+        return account
+
+    @classmethod
+    def get_by_password(cls, password):
+        secretPass = cls.query.filter_by(password=password).one_or_none()
+        return secretPass
+
+    @classmethod
+    def get_all(cls):
+        users = cls.query.all()
+        return users
+        
+
+    @classmethod
+    def get_by_id(cls, id):
+        user_id = cls.query.get(id)
+        return user_id
+
+
+    def add_fav_starship (self,starship):
+        self.have_user_starship.append(starship)
+        return self.have_user_starship
+
+
+class Starship(db.Model):
+    __tablename__ : 'starship'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=False, nullable=False)
+    starships_id = db.Column(db.Integer, db.ForeignKey("starship_detail.id"), nullable=False)
+
+    starship_has_details = db.relationship("StarshipDetail", back_populates = "starship_detail_has_starship")
+    have_user_starshipfav = db.relationship("User", secondary=starshipfavourites, back_populates = "have_user_starship")
+    
+
+    def __repr__(self):
+        return f'Starship is starships_id:{self.starship_id}, id:{self.id}, name:{self.name}'
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name":self.name,
+            "starships_id": self.starships_id
             # do not serialize the password, its a security breach
         }
+    @classmethod
+    def get_all(cls):
+        starships = cls.query.all()
+        return starships
+
+    @classmethod
+    def get_by_id(cls, id):
+        starship_id = cls.query.get(id)
+        return starship_id
+
+class StarshipDetail(db.Model):
+    __tablename__ : 'starship_detail'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=False, nullable=False)
+    model = db.Column(db.Integer, unique=False, nullable=False)
+    starship_class = db.Column(db.String(120), unique=False, nullable=False)
+    manufacturer = db.Column(db.String(120), unique=False, nullable=False)
+    
+
+    starship_detail_has_starship = db.relationship("Starship", back_populates = "starship_has_details")
+
+    def __repr__(self):
+        return f'StarshipDetail is id:{self.id}, name: {self.name}, model: {self.model}, starship_class: {self.starship_class}, manufacturer:{self.manufacturer}, cost_in_credits:{self.cost_in_credits}, length:{self.length}, max_atmosphering_speed:{self.max_atmosphering_speed}, crew:{self.crew}, passengers:{self.passengers}, pilots:{self.pilots}, cargo_capacity:{self.cargo_capacity}, consumables:{self.consumables}, hyperdrive_rating:{self.hyperdrive_rating}, mlgt:{self.mlgt}, pilots:{self.films}, films:{self.films}, created:{self.created}, edited:{self.edited}, url:{self.url}'
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "model": self.model,
+            "starship_class": self.starship_class,
+            "manufacturer": self.manufacturer,
+        }
+
+@classmethod
+def get_by_id(cls, id):
+    starship = cls.query.get(id)
+    return starship
